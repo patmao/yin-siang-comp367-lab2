89a6757a62d4594b1babc87f145bd6cbb786ea2e
Yin-Siang Mao <ymao6@my.centennialcollege.ca>
2024-02-22 15:04:06 -0500

    first commit

---
+++ src/main/java/com/ym/lab2/YinSiangMaoComp367Lab2Application.java
@@ -0,0 +1,13 @@
+package com.ym.lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinSiangMaoComp367Lab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YinSiangMaoComp367Lab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ym/lab2/YinSiangMaoComp367Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ym.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinSiangMaoComp367Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
